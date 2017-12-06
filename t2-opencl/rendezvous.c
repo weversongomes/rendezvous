7e362b997e1df8629f970b0dfc801beef0bfcc60
Weverson <weverson_santos@live.com>
2017-12-06 16:38:12 -0300

    Arquivos solução do problema 2

---
+++ t2-opencl/rendezvous.c
@@ -0,0 +1,177 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+     
+    #ifdef __APPLE__
+    #include <OpenCL/opencl.h>
+    #else
+    #include <CL/cl.h>
+    #endif
+     
+    #define MAX_SOURCE_SIZE (0x100000)
+    #define LENGTH (100)
+    #define X (100)
+    #define gama (17)
+    #define Ve (10)
+    
+    float x=0, y=0, xl0=0, yl0=0;
+     
+    int main()
+    {
+    cl_device_id device_id = NULL;
+    cl_context context = NULL;
+    cl_command_queue command_queue = NULL;
+    cl_mem d_a = NULL, d_b = NULL, d_c = NULL;
+    cl_program program = NULL;
+    cl_kernel kernel = NULL;
+    cl_platform_id platform_id = NULL;
+    cl_uint ret_num_devices;
+    cl_uint ret_num_platforms;
+    cl_int ret;
+     
+    FILE *fp;
+    char fileName[] = "./rendezvous.cl";
+    char *source_str;
+    size_t source_size;
+    float* h_a = (float*) calloc(LENGTH, sizeof(float));   // a vector
+    float* h_b = (float*) calloc(LENGTH, sizeof(float));   // b vector
+    float* h_c = (float*) calloc(LENGTH, sizeof(float));   // c vector
+     
+    /* Load the source code containing the kernel*/
+    fp = fopen(fileName, "r");
+    if (!fp) {
+        fprintf(stderr, "Failed to load kernel.\n");
+        exit(1);
+    }
+    source_str = (char*)malloc(MAX_SOURCE_SIZE);
+    source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
+    fclose(fp);
+    
+    h_a[0] = 1;
+    h_a[1] = 20;
+    h_a[2] = 3;
+    h_a[3] = 4;
+    h_a[4] = 5;
+    
+    h_b[0] = 1;
+    h_b[1] = 20;
+    h_b[2] = 3;
+    h_b[3] = 4;
+    h_b[4] = 5;
+    
+    /* Get Platform and Device Info */
+    ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
+    ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
+     
+    /* Create OpenCL context */
+    context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
+     
+    /* Create Command Queue */
+    command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
+     
+    /* Create Memory Buffer */
+    d_a = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * LENGTH, NULL, &ret);
+    d_b = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * LENGTH, NULL, &ret);
+    d_c = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(float) * LENGTH, NULL, &ret);
+    // read/write = CL_MEM_READ_WRITE
+     
+    /* Create Kernel Program from the source */
+    program = clCreateProgramWithSource(context, 1, (const char **)&source_str,
+    (const size_t *)&source_size, &ret);
+     
+    /* Build Kernel Program */
+    ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+    
+    /* Create OpenCL Kernel */
+    kernel = clCreateKernel(program, "main", &ret);
+    
+    //--------------------------------------
+    FILE *arq;
+    char url[] = "in.dat";
+    arq = fopen(url, "r");
+    float var1;
+	float done = 0; // usada para mostrar o percentual calculado
+
+	/*printf("0%c concluido\r", 37);
+	fflush(stdout);*/
+    int NPI = 5;
+    for(int np = 1; np <= NPI; np++) {
+        
+        if(arq == NULL) {
+            printf("Erro, nao foi possivel abrir o arquivo\n");
+            exit(EXIT_FAILURE);
+        } else {
+            fscanf(arq,"%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f\n", &var1, &var1, &var1, &x, &y, &var1, &var1, &xl0, &yl0, &var1, &var1, &var1, &var1, &var1, &var1, &var1, &var1, &var1, &var1);
+        }
+    
+        h_a[0] = x;
+        h_a[1] = y;
+        h_a[2] = xl0;
+        h_a[3] = yl0;
+
+        // Write a and b vectors into compute device memory
+        ret = clEnqueueWriteBuffer(command_queue, d_a, CL_TRUE, 0, sizeof(float) * LENGTH, h_a, 0, NULL, NULL);
+        if (ret != CL_SUCCESS)
+        {
+            printf("Error: 1! %d\n", ret);
+            exit(1);
+        }
+        ret = clEnqueueWriteBuffer(command_queue, d_b, CL_TRUE, 0, sizeof(float) * LENGTH, h_b, 0, NULL, NULL);
+        if (ret != CL_SUCCESS)
+        {
+            printf("Error: 2! %d\n", ret);
+            exit(1);
+        }
+        
+        /* Set OpenCL Kernel Parameters */
+        /*ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&d_a);
+        ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&d_b);
+        ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&d_c);*/
+        
+        ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_a);
+        ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_b);
+        ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_c);
+        
+        /* Execute OpenCL Kernel */
+        size_t global[3] = {10, 100, 17};
+        //ret = clEnqueueTask(command_queue, kernel, 0, NULL,NULL);
+        ret = clEnqueueNDRangeKernel(command_queue, kernel, 3, NULL, global, NULL, 0, NULL, NULL);
+        if (ret != CL_SUCCESS)
+        {
+            printf("Error: Failed to EnqueueNDRangeKernel! %d\n", ret);
+            exit(1);
+        }
+        
+        /* Copy results from the memory buffer */
+        //ret = clEnqueueReadBuffer(command_queue, d_c, CL_TRUE, 0, MEM_SIZE * sizeof(char), h_c, 0, NULL, NULL);
+        ret = clEnqueueReadBuffer(command_queue, d_c, CL_TRUE, 0, sizeof(float) * LENGTH, h_c, 0, NULL, NULL);
+        if (ret != CL_SUCCESS)
+        {
+            printf("Error: Failed to read output array! %d\n", ret);
+            exit(1);
+        }
+        
+        /* Display Results */        
+        for(int t = 0; t < 10; t++) {
+            printf("%f\n", h_c[t]);
+        }
+        printf("--------------------------------------\n");
+    }
+     
+    /* Finalization */
+    ret = clFlush(command_queue);
+    ret = clFinish(command_queue);
+    ret = clReleaseKernel(kernel);
+    ret = clReleaseProgram(program);
+    ret = clReleaseMemObject(d_a);
+    ret = clReleaseMemObject(d_b);
+    ret = clReleaseMemObject(d_c);
+    ret = clReleaseCommandQueue(command_queue);
+    ret = clReleaseContext(context);
+     
+    free(source_str);
+    free(h_a);
+    free(h_b);
+    free(h_c);
+     
+    return 0;
+    }
---
+++ t2-opencl/rendezvous.cl
@@ -0,0 +1,154 @@
+float brute_A (float y0, float xl0, float gama, float X, float vex, float vey, float gamavey_ww, float gama_w, float vex2_w, float w)
+{
+    float result = 0;
+    float aux;
+    float sum = 0;
+
+    result = (2*xl0)/w - 3*y0 + ((2*vex)/w) * log((X+1)/X);
+
+    //Calculo do somatorio
+    for (int n = 1; n <= 20; n++) {
+        aux = (1/(n*pow(X, n)))*(1/(1+(n*gama_w)*(n*gama_w)))*((vex2_w)+(n*gamavey_ww));
+        if (n%2 == 0) {//iteracao par
+            aux = -aux;
+        }
+        sum += aux;
+    }
+    result -= sum;
+
+    return result;
+}
+
+float brute_B (float yl0, float X, float vey, float gamavex_ww, float gama_wpow, float vey_w, float w) {
+    float result = 0;
+    float sum = 0;
+    float aux;
+
+    result = yl0/w + (vey/w)*log((X+1)/X);
+
+    // Calculo do somatorio
+    for (int n = 1; n <= 20; n++) {
+        aux = (1/(n*pow(X,n)))*(1/(1+(n*n*gama_wpow)))*(vey_w + (n*gamavex_ww));
+        if (n%2 == 0) {//iteracao par
+            aux = -aux;
+        }
+        sum += aux;
+    }
+
+    result+= sum;
+    return result;
+}
+
+float brute_E (float y0, float xl0, float X, float vex, float w) {
+    float result = 0;
+
+    result -= 3*vex*log((X+1)/X);
+    result += 6*w*y0 - 3*xl0;
+
+    return result;
+}
+
+float brute_G (float x0, float yl0, float X, float vex, float vey, float w) {
+    float result = 0;
+    float sum = 0;
+    float aux;
+
+    result= 2*yl0/w + x0 + (2*vey*(log((X+1)/X)))/w;
+    
+    for (int n = 1; n <= 20; n++) {
+        aux = (vex*3)/(n*n*pow(X,n)*w);
+
+        if (n%2 == 0) {
+            aux = -aux;
+        }
+        sum +=aux;
+    }
+
+    result-=sum;
+
+    return result;
+}
+
+float dX (float t, float vex, float gama, float X, float A, float B, float E, float G, float vey2_w, float gama_wpow, float w) {
+    //otimizacao
+    float wt = w*t;
+
+    float resultFn = 0.0f;
+    float result1 = 2.0f * (A*sin(wt)-B*cos(wt))+E*t;
+    
+    float result2 = G;
+
+    for (int n = 1; n <= 20; n++) {
+        // brute_F
+        resultFn = (1/(n*pow(X,n)))*(vey2_w + (vex*4)/(n*gama))/(1+(n*n*gama_wpow));
+
+        if (n%2 == 0) {
+            resultFn = -resultFn;
+        }
+        resultFn -= vex/(n*gama);
+        //brute_F
+
+        result2 += resultFn * pow(M_E, -(n * gama*t));
+    }
+    return result1 + result2;
+}
+    
+__kernel void main(__global float* a, __global float* b, __global float* c)
+{
+    int Ve = get_global_id(0);
+    int X = get_global_id(1);
+    int expGama = get_global_id(2);
+    float w = 0.743737;
+    float gama;
+    float vex, vey;
+    float vex2_w, vey2_w;
+    //otimizacao -------------------------------
+    //brute_A
+    float gama_w;
+    //brute_B
+    float gamavex_ww;
+    //dy vy A
+    float gamavey_ww;
+    //vx vy vz dx dy dz B H I
+    float gama_wpow;
+    float A = 0.0f, B = 0.0f, E = 0.0f, G = 0.0f;
+    float VeReal = 0.0f;
+    float response = 5.0f;
+    //------------------------------------------
+    c[0] = get_global_size(0);
+    c[1] = get_global_size(1);
+    c[2] = get_global_size(2);
+    c[3] = a[0];
+    c[4] = a[1];
+    c[5] = a[2];
+    c[6] = a[3];
+    if(Ve < 10 && X < 100 && expGama < 17) {
+        VeReal = 0.5f * (Ve + 1);
+        gama = pown(10.0f, expGama - 14);
+        vex = vey = VeReal*VeReal/3;
+        vex2_w = vey2_w = (2*vex)/w;
+        gama_w = gama/w;
+        gamavex_ww = gamavey_ww = (gama*vex)/(w*w);
+        gama_wpow = (gama/w)*(gama/w);
+        
+        A = brute_A (a[1], a[2], gama, X+1, vex, vey, gamavey_ww, gama_w, vex2_w, w);
+        B = brute_B (a[3], X+1, vey, gamavex_ww, gama_wpow, vex2_w, w);
+        E = brute_E (a[1], a[2], X+1, vex, w);
+        G = brute_G (a[0], a[3], X+1, vex, vey, w);
+        for(int t = 0; t < 86400; t++) {
+            if (t == 5 && Ve == 5.0f && X == 55.0f && expGama == 16.0f) {
+                response = dX(t, vex, gama, X+1, A, B, E, G, vey2_w, gama_wpow, w);
+            } else {
+                dX(t, vex, gama, X+1, A, B, E, G, vey2_w, gama_wpow, w);
+            }
+        }
+        
+        if(Ve == 5 && X == 56 && expGama == 16) {
+            c[7] = 0.0f;
+        } else if(Ve == 5 && X == 54 && expGama == 16) {
+            c[8] = 0.0f;
+        } else if(Ve == 5 && X == 55 && expGama == 16) {
+            c[9] = response;
+        }
+    }
+}
